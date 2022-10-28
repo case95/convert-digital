ff317d361640028c498cad6db99c4b2e60092c6a
Lorenzo <lorenzodr.design@gmail.com>
2022-10-29 10:21:39 +1100

    🔐 Exporting secrets shared with client

---
+++ next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  env: {
+    SHOP: process.env.SHOP,
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN:
+      process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
+  },
+  images: {
+    domains: ["cdn.shopify.com"],
+  },
+};
